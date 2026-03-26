837935d5e4fb291414c7c96f9c9055d6a6ca3ca6
savich18423 <savich618@gmail.com>
2026-03-27 00:53:16 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/sasavot_mafia/', // Добавляем базовый путь для GitHub Pages
+    base: '/mafia_sasavot/', // Исправляем базовый путь для GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
